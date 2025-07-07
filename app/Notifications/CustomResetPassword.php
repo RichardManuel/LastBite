ee695013ace8459ddb55d701be6248d410799dc7
Shawwnk <sean.kurniawan@binus.ac.id>
2025-07-07 14:26:37 +0700

    commit 4 notif

---
+++ app/Notifications/CustomResetPassword.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class CustomResetPassword extends Notification
+{
+    use Queueable;
+
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     */
+
+    public $token;
+
+    public function __construct($token)
+    {
+        $this->token = $token;
+    }
+    public function toMail(object $notifiable): MailMessage
+    {
+        $resetUrl = url(route('password.reset', [
+            'token' => $this->token,
+            'email' => $notifiable->getEmailForPasswordReset(),
+        ], false));
+
+        // ganti url importan!!!!
+        return (new MailMessage)
+            ->subject('Reset Password - LastBite')
+            ->greeting('Hello ' . $notifiable->name . ' 👋')
+            ->line('We received a request to reset your password.')
+            ->action('Reset Password', $resetUrl)
+            ->line('If you didn’t request this, no further action is required.')
+            ->salutation('Stay healthy, LastBite Team');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(object $notifiable): array
+    {
+        return [
+            //
+        ];
+    }
+}
