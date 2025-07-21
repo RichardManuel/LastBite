931314176aa1c0e81f6ca91a83821759c99e2c6e
RichardManuel <richard123yes@gmail.com>
2025-07-21 13:40:53 +0700

    dsssds

--- app/Http/Controllers/admin/RestaurantManagementController.php
+++ app/Http/Controllers/admin/RestaurantManagementController.php
@@ -1,10 +1,9 @@
-    <?php
+<?php
+namespace App\Http\Controllers\admin;
 
-    namespace App\Http\Controllers\admin;
-
-    use App\Http\Controllers\Controller;
-    use App\Models\Restaurant;
-    use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Models\Restaurant;
+use Illuminate\Http\Request;
 
     class RestaurantManagementController extends Controller
     {
