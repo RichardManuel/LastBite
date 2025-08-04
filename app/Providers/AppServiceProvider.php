5e314e8c85ef3ca81debaef56f8671516163d919
RichardManuel <169007853+RichardManuel@users.noreply.github.com>
2025-08-04 07:39:31 +0700

    deploy

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,25 +3,24 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\Route;  // Add this import
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\URL; // Jangan lupa ini
 use App\Models\Restaurant;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register()
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot()
     {
-        // Binding the 'restaurant_id' parameter to the Restaurant model
+        if (config('app.env') === 'production') {
+            URL::forceScheme('https');
+        }
+
+        // Binding the 'restaurant_id' parameter
         Route::bind('restaurant_id', function ($value) {
             return Restaurant::where('restaurant_id', $value)->firstOrFail();
         });
